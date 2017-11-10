0e6a80c3440a4ff16b7bd31399253f6167f48809
Alexander Kolbin <kolbin_aa@yahoo.com>
2017-11-10 11:52:49 -0500

    feat: Create base of UserVC at storyboard and create UserVC class

---
+++ Targeter/UserVC.swift
@@ -0,0 +1,16 @@
+//
+//  UserVC.swift
+//  Targeter
+//
+//  Created by mac on 11/10/17.
+//  Copyright © 2017 Alder. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import Firebase
+
+class UserViewController: UIViewController {
+    
+    
+}
