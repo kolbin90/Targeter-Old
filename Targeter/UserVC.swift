0e53d2672a19af65a567d77a5eff799c78a51428
Alexander Kolbin <kolbin_aa@yahoo.com>
2017-11-19 21:02:00 -0500

    feat: Logout with button at account

--- Targeter/UserVC.swift
+++ Targeter/UserVC.swift
@@ -13,4 +13,14 @@ import Firebase
 class UserViewController: UIViewController {
     
     
+    // MARK: Actions
+    @IBAction func logoutButton(_ sender: Any) {
+        do {
+            try Auth.auth().signOut()
+            _ = navigationController?.popViewController(animated: true)
+        } catch {
+            print("unable to sign out: \(error)")
+        }
+        
+    }
 }
