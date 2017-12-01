eec4f3323ce0bf966564f43f22f60f7f95ba2f47
Alexander Kolbin <kolbin_aa@yahoo.com>
2017-11-30 22:45:34 -0500

    feat: Create EditProfileVC class and storyboard

---
+++ Targeter/EditProfileVC.swift
@@ -0,0 +1,18 @@
+//
+//  EditProfileVC.swift
+//  Targeter
+//
+//  Created by mac on 11/30/17.
+//  Copyright © 2017 Alder. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class EditProfileViewController: UIViewController {
+    
+    // MARK: Outlets
+    
+    // MARK: Actions
+    
+}
