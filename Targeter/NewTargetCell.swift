7d8f2d1da6a06dff4d3dccaaf6c213ae0ee27e8e
Alexander Kolbin <kolbin_aa@yahoo.com>
2017-12-01 22:39:25 -0500

    feat: Create NewTargetCell class and nib

---
+++ Targeter/NewTargetCell.swift
@@ -0,0 +1,24 @@
+//
+//  NewTargetCell.swift
+//  Targeter
+//
+//  Created by mac on 12/1/17.
+//  Copyright © 2017 Alder. All rights reserved.
+//
+
+import UIKit
+
+class NewTargetCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
