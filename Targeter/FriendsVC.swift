f08eed0df39d7a9f0d8dab0707fdec3923122d01
Alexander Kolbin <kolbin_aa@yahoo.com>
2017-12-02 19:24:07 -0500

    feat: Create prototype of new design for target

--- Targeter/FriendsVC.swift
+++ Targeter/FriendsVC.swift
@@ -13,8 +13,9 @@ import Firebase
 class FriendsViewController: UITableViewController {
     // UITableViewControllerDelegate & DataSource
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as UITableViewCell
-        return cell
+        //let cell = tableView.dequeueReusableCell(withIdentifier: "NewTargetCell", for: indexPath) as! NewTargetCell
+        let newCell = Bundle.main.loadNibNamed("NewTargetCell", owner: nil, options: nil)!.first as! NewTargetCell
+        return newCell
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
