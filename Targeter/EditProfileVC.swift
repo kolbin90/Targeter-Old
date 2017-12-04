f18966f043d770785e59b75d6ac2bb18ae71ee46
Alexander Kolbin <kolbin_aa@yahoo.com>
2017-12-03 20:54:52 -0500

    feat: Configure Database in EditProfileVC

--- Targeter/EditProfileVC.swift
+++ Targeter/EditProfileVC.swift
@@ -8,22 +8,45 @@
 
 import Foundation
 import UIKit
+import Firebase
+import FirebaseAuthUI
 
 class EditProfileViewController: UIViewController {
     
+    // MARK: Properties
+    var userID = Auth.auth().currentUser?.uid
+    var ref:DatabaseReference!
     // MARK: Outlets
-    
     @IBOutlet weak var profileImageView: UIImageView!
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var cityTextField: UITextField!
     @IBOutlet weak var ageTextField: UITextField!
     @IBOutlet weak var aboutTextField: UITextField!
     
+    // MARK: - LifeCicle
+    override func viewDidLoad() {
+        configDatabase()
+    }
+    
     // MARK: - Delegates
     // MARK: TextField Delegate
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         dismissKeyboard()
         return true
+    }
+    // MARK: Assist functions
+    func configDatabase(){
+        ref = Database.database().reference()
+        if let userID = userID {
+            ref.child("users").child(userID).observeSingleEvent(of: .value, with: { (snapshot) in
+                // Get user value
+                let value = snapshot.value as? NSDictionary
+                let userName = value?["username"] as? String ?? ""
+                self.nameTextField.text = userName
+            }) { (error) in
+                print(error.localizedDescription)
+            }
+        }
     }
     // MARK: Actions
     
