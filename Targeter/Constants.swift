1df0ec93e724840e6a9f3b074754a5a3a4db1081
Alexander Kolbin <kolbin_aa@yahoo.com>
2017-12-03 21:11:00 -0500

    feat: Create Constant file and struct

---
+++ Targeter/Constants.swift
@@ -0,0 +1,19 @@
+//
+//  Constants.swift
+//  Targeter
+//
+//  Created by mac on 12/3/17.
+//  Copyright © 2017 Alder. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    struct UserData {
+        static let name = "name"
+        static let city = "city"
+        static let age = "age"
+        static let about = "about"
+        static let imageURL = "imageURL"
+    }
+}
