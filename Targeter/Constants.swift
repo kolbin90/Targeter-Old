e8cd54741d4f920a4afc128fb4ba2e97fab086ad
Alexander Kolbin <kolbin_aa@yahoo.com>
2018-03-19 20:00:20 -0400

    feat: Add constraints for Firebase

--- Targeter/Constants.swift
+++ Targeter/Constants.swift
@@ -17,4 +17,18 @@ struct Constants {
         static let imageURL = "imageURL"
         static let username = "username"
     }
+    struct Target {
+        static let title = "title"
+        static let description = "description"
+        static let percentage = "percentage"
+        static let started = "started"
+        static let completed = "completed"
+        static let imageURL = "imageURL"
+        static let checkins = "checkins"
+        static let dateBeginning = "dateBeginning"
+    }
+    struct RootFolders {
+        static let users = "users"
+        static let targets = "targets"
+    }
 }
