266623c78a35408d1d3f429add80a38e887f4037
Alexander Kolbin <kolbin_aa@yahoo.com>
2018-03-25 17:25:48 -0400

    feat: Download target data from firebase

--- Targeter/FriendsVC.swift
+++ Targeter/FriendsVC.swift
@@ -12,8 +12,57 @@ import Firebase
 
 class FriendsViewController: UITableViewController {
     
+    //MARK: - Properties
+    var userID = Auth.auth().currentUser?.uid
+    var databaseRef: DatabaseReference!
+    var storageRef: StorageReference!
+    var targets:[DataSnapshot]! = []
+    fileprivate var _refHandle: DatabaseHandle!
+
     
-    // UITableViewControllerDelegate & DataSource
+    //MARK: - Outlets
+    
+    
+    // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Set up Navigation controller
+        configDatabase()
+        configureStorage()
+        downloadTargets()
+    }
+    deinit {
+        // TODO: set up what needs to be deinitialized when view is no longer being used
+        if let userID = userID {
+            databaseRef.child(Constants.RootFolders.Targets).child(userID).removeObserver(withHandle: _refHandle)
+        }
+    }
+    // MARK: - Delegates
+    
+    // MARK: Firebase functions
+    func configDatabase(){
+        databaseRef = Database.database().reference()
+    }
+    func configureStorage() {
+        storageRef = Storage.storage().reference()
+    }
+    func downloadTargets() {
+        if let userID = userID {
+            _refHandle = databaseRef.child(Constants.RootFolders.Targets).child(userID).observe(.childAdded, with: { (snapshot) in
+                self.targets.append(snapshot)
+                let value = snapshot.value as? [String:AnyObject]
+                self.tableView.reloadData()
+                //print(value?.allValues[0])
+                //let targetValue
+               // print(Array(value!.values)[0])
+                
+            }) { (error) in
+                print(error.localizedDescription)
+            }
+        }
+    }
+    
+    // MARK: UITableViewControllerDelegate & DataSource
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         //let cell = tableView.dequeueReusableCell(withIdentifier: "NewTargetCell", for: indexPath) as! NewTargetCell
         let newCell = Bundle.main.loadNibNamed("NewTargetCell", owner: nil, options: nil)!.first as! NewTargetCell
@@ -21,7 +70,7 @@ class FriendsViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return targets.count
         
     }
     
