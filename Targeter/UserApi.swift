c0dd20b823e6cebd36ca37217b2b3e3c388e6d3e
Alexander Kolbin <kolbin_aa@yahoo.com>
2019-12-09 02:27:02 -0500

    feat: Create functions singleObserveCurrentUser and singleObserveUser

--- Targeter/UserApi.swift
+++ Targeter/UserApi.swift
@@ -8,12 +8,32 @@
 
 import Foundation
 import FirebaseAuth
+import FirebaseDatabase
 
 class UserApi {
+    
+    var usersRef = Database.database().reference().child(Constants.RootFolders.Users)
     var currentUser: User? {
         if let currentUser = Auth.auth().currentUser {
             return currentUser
         }
         return nil
+    }
+    
+    func singleObserveCurrentUser(completion: @escaping (UserModel) -> Void){
+        guard let userId = currentUser?.uid else {
+            return
+        }
+        singleObserveUser(withUid: userId, completion: completion)
+    }
+    
+    func singleObserveUser(withUid uid: String, completion: @escaping (UserModel) -> Void) {
+        usersRef.child(uid).observeSingleEvent(of: .value) { (snapshot) in
+           // print(snapshot.value)
+            if let dict = snapshot.value as? [String: Any] {
+                let user = UserModel.transformDataToUser(dict: dict)
+                completion(user)
+            }
+        }
     }
 }
