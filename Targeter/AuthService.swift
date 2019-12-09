5d139b77df8dde76da909117edbeb32b8e8e255a
Alexander Kolbin <kolbin_aa@yahoo.com>
2019-12-09 00:01:46 -0500

    feat: Make optional function variables

--- Targeter/AuthService.swift
+++ Targeter/AuthService.swift
@@ -17,7 +17,7 @@ import CoreData
 class AuthService {
     
     let stack = (UIApplication.shared.delegate as! AppDelegate).stack
-
+    
     static func firebaseLogOut() {
         do {
             // Trying to sign out from Firebase
@@ -41,43 +41,49 @@ class AuthService {
         graphRequestConnection.start()
     }
     
-    static func saveNewUserInfo(profileImageUrl: String, name: String, username: String) {
+    static func saveNewUserInfo(profileImageUrl: String?, name: String?, username: String?) {
         let storageRef = Storage.storage().reference()
         let databaseRef = Database.database().reference()
-        let imagePath = "users/" + Auth.auth().currentUser!.uid + "/profileImage/\(Date())"
-        let metadata = StorageMetadata()
-        metadata.contentType = "image/jpeg"
-        
-        // Download image from Facebook, Upload it to Firevase storage, save imageURL to database
-        guard let url = URL(string: profileImageUrl) else {
-            return
-        }
-        let urlRequest = URLRequest(url: url)
-        URLSession.shared.dataTask(with: urlRequest) { (data, responce, error) in
-            guard error == nil else {
-                ProgressHUD.showError(error?.localizedDescription)
-                return
-            }
-            guard let data = data else {
-                return
-            }
+        if let profileImageUrl = profileImageUrl {
+
+            let imagePath = "users/" + Auth.auth().currentUser!.uid + "/profileImage/\(Date())"
+            let metadata = StorageMetadata()
+            metadata.contentType = "image/jpeg"
             
-            DispatchQueue.main.async {
-                storageRef.child(imagePath).putData(data, metadata: metadata, completion: { (storageMetafata, error) in
-                    guard (error == nil) else {
-                        print("error saving image to storage")
-                        return
-                    }
-                    
-                    let imageURL = storageRef.child((storageMetafata?.path)!).description
-                    databaseRef.child("users").child(Auth.auth().currentUser!.uid).updateChildValues([Constants.UserData.ImageURL:imageURL])  //child(Constants.UserData.ImageURL).setValue(imageURL)
-                })
+            // Download image from Facebook, Upload it to Firevase storage, save imageURL to database
+            guard let url = URL(string: profileImageUrl) else {
+                return
             }
-            }.resume()
+            let urlRequest = URLRequest(url: url)
+            URLSession.shared.dataTask(with: urlRequest) { (data, responce, error) in
+                guard error == nil else {
+                    ProgressHUD.showError(error?.localizedDescription)
+                    return
+                }
+                guard let data = data else {
+                    return
+                }
+                
+                DispatchQueue.main.async {
+                    storageRef.child(imagePath).putData(data, metadata: metadata, completion: { (storageMetafata, error) in
+                        guard (error == nil) else {
+                            print("error saving image to storage")
+                            return
+                        }
+                        
+                        let imageURL = storageRef.child((storageMetafata?.path)!).description
+                        databaseRef.child("users").child(Auth.auth().currentUser!.uid).updateChildValues([Constants.UserData.ImageURL:imageURL])  //child(Constants.UserData.ImageURL).setValue(imageURL)
+                    })
+                }
+                }.resume()
+        }
         // Save username and name to database
-        databaseRef.child("users").child(Auth.auth().currentUser!.uid).child(Constants.UserData.Username).setValue(username)
-        databaseRef.child("users").child(Auth.auth().currentUser!.uid).child(Constants.UserData.Name).setValue(name)
-       
+        if let username = username {
+            databaseRef.child("users").child(Auth.auth().currentUser!.uid).child(Constants.UserData.Username).setValue(username)
+        }
+        if let name = name {
+            databaseRef.child("users").child(Auth.auth().currentUser!.uid).child(Constants.UserData.Name).setValue(name)
+        }
     }
     
 }
