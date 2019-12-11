88b689580b5c20fece8d0c9e34e47f9ba53d50ee
Alexander Kolbin <kolbin_aa@yahoo.com>
2019-12-10 21:47:13 -0500

    feat: Create function isValidEmail

--- Targeter/SignUpViewController.swift
+++ Targeter/SignUpViewController.swift
@@ -15,8 +15,12 @@ class SignUpViewController: UIViewController {
 
         // Do any additional setup after loading the view.
     }
-    
-    
+    func isValidEmail(emailStr:String) -> Bool {
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        
+        let emailPred = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+        return emailPred.evaluate(with: emailStr)
+    }
 
     /*
     // MARK: - Navigation
