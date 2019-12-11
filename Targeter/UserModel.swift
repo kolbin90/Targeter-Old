e73d2814edb9a4b02d6b0ac42336ef3c32210860
Alexander Kolbin <kolbin_aa@yahoo.com>
2019-12-10 20:23:06 -0500

    feat: Add usernameLowercased and id to UserModel

--- Targeter/UserModel.swift
+++ Targeter/UserModel.swift
@@ -11,7 +11,9 @@ class UserModel {
     var name: String?
     var imageURLString: String?
     var username: String?
+    var usernameLowercased: String?
     var email: String?
+    var id: String?
 }
 extension UserModel {
     static func transformFaceBookDataToUser(dict: [String:Any]) -> UserModel {
@@ -30,10 +32,13 @@ extension UserModel {
         return user
     }
     
-    static func transformDataToUser(dict: [String:Any]) -> UserModel {
+    static func transformDataToUser(dict: [String:Any], id: String) -> UserModel {
         let user = UserModel()
         user.email = dict[Constants.UserData.Email] as? String
         user.username = dict[Constants.UserData.Username] as? String
+        user.usernameLowercased = dict[Constants.UserData.UsernameLowercased] as? String
+        user.id = id
+
         user.name = dict[Constants.UserData.Name] as? String
         user.imageURLString = dict[Constants.UserData.ImageURL] as? String
         return user
