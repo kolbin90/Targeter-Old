e4dd63932ed71c9cb7dbb812a26e0f1483b98f9d
Alexander Kolbin <kolbin_aa@yahoo.com>
2020-01-15 17:32:38 -0500

    feat: Create class CheckInModel, create custom initialiser

---
+++ Targeter/CheckInModel.swift
@@ -0,0 +1,25 @@
+//
+//  CheckInModel.swift
+//  Targeter
+//
+//  Created by Apple User on 1/15/20.
+//  Copyright © 2020 Alder. All rights reserved.
+//
+
+import Foundation
+
+class CheckInModel {
+    var result: String?
+    var timestamp: Int?
+}
+
+extension CheckInModel {
+    static func transformDataToCheckIn(dict: [String:Any], id: String) -> CheckInModel {
+        
+        let checkIn = CheckInModel()
+        checkIn.result = dict[Constants.CheckIn.Result] as? String
+        checkIn.timestamp = dict[Constants.CheckIn.Timestamp] as? Int
+
+        return checkIn
+    }
+}
