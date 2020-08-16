c6c2e2408d789c4a22cccaaf84427e15d00166c0
Alexander Kolbin <kolbin_aa@yahoo.com>
2020-08-15 22:12:42 -0400

    feat: Create EditProfileViewController file and class

---
+++ Targeter/EditProfileViewController.swift
@@ -0,0 +1,13 @@
+//
+//  EditProfileViewController.swift
+//  Targeter
+//
+//  Created by Alexander Kolbin on 8/15/20.
+//  Copyright © 2020 Alder. All rights reserved.
+//
+
+import Foundation
+
+class EditProfileViewController: UITableViewController {
+    
+}
