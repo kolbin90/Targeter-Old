c126f95be955bc498dad2e75270dfce8d9b65024
Alexander Kolbin <kolbin_aa@yahoo.com>
2020-09-16 17:02:02 -0400

    feat: Add outlets and actions

--- Targeter/EditProfileViewController.swift
+++ Targeter/EditProfileViewController.swift
@@ -10,4 +10,18 @@ import Foundation
 
 class EditProfileViewController: UITableViewController {
     
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var locationTextField: UITextField!
+    
+    @IBAction func chooseImageButton_TchUpIns(_ sender: Any) {
+    }
+    @IBAction func saveButton_TchUpIns(_ sender: Any) {
+    }
+    
+    @IBAction func cancelButton_TchUpIns(_ sender: Any) {
+    }
+    
 }
