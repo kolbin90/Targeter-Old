2026f1d9af5c593465d2c8483bc5e14ae17bdda3
Alexander Kolbin <kolbin_aa@yahoo.com>
2020-09-16 22:55:22 -0400

    feat: Add outlets

--- Targeter/ProfileCell.swift
+++ Targeter/ProfileCell.swift
@@ -9,8 +9,19 @@
 import UIKit
 
 class ProfileCell: UITableViewCell {
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var targetsLabel: UILabel!
+    @IBOutlet weak var pointsLabel: UILabel!
+    @IBOutlet weak var followersLabel: UILabel!
+    @IBOutlet weak var followingLabel: UILabel!
+    @IBOutlet weak var profileImageView: UIImageView!
+    
     @IBOutlet weak var headerView: UIView!
     @IBOutlet weak var headerViewHeightConstraint: NSLayoutConstraint!
+    
+    
     
     override func awakeFromNib() {
         super.awakeFromNib()
