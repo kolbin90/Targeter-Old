1340d62225a7ea60ea780a60130220cd3d369293
Alexander Kolbin <kolbin_aa@yahoo.com>
2020-09-29 16:44:13 -0400

    feat: Create FeedCell

---
+++ Targeter/FeedCell.swift
@@ -0,0 +1,24 @@
+//
+//  FeedCell.swift
+//  Targeter
+//
+//  Created by Alexander Kolbin on 9/28/20.
+//  Copyright © 2020 Alder. All rights reserved.
+//
+
+import UIKit
+
+class FeedCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
