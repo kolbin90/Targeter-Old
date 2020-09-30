222cdf4e7b6d8bd87774d17674d26580a640236f
Alexander Kolbin <kolbin_aa@yahoo.com>
2020-09-30 19:37:16 -0400

    feat: Create FeedVC class

---
+++ Targeter/FeedViewController.swift
@@ -0,0 +1,47 @@
+//
+//  FeedViewController.swift
+//  Targeter
+//
+//  Created by Alexander Kolbin on 9/30/20.
+//  Copyright © 2020 Alder. All rights reserved.
+//
+
+import UIKit
+
+class FeedViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(UINib(nibName: "FeedCell", bundle: nil), forCellReuseIdentifier: "FeedCell")
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+extension FeedViewController:  UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //let user = users[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "FeedCell", for: indexPath) as! FeedCell
+//
+        return cell
+    }
+}
