b8cc355257c542b871b42aec0ec63432fe7ce6c6
Alexander Kolbin <kolbin_aa@yahoo.com>
2020-10-04 16:35:35 -0400

    feat: Create class PostModel

---
+++ Targeter/PostModel.swift
@@ -0,0 +1,14 @@
+//
+//  PostModel.swift
+//  Targeter
+//
+//  Created by Alexander Kolbin on 10/4/20.
+//  Copyright © 2020 Alder. All rights reserved.
+//
+
+import Foundation
+
+class PostModel {
+    var user: UserModel!
+    var target: TargetModel!
+}
