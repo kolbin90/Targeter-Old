844f8c8f812d21bfb63f658485814f456f7f2c5c
Alexander Kolbin <kolbin_aa@yahoo.com>
2021-06-25 14:46:18 -0400

    feat: Add likes increasing functions

--- Targeter/LikesApi.swift
+++ Targeter/LikesApi.swift
@@ -11,8 +11,47 @@ import FirebaseDatabase
 
 class LikesApi {
     let likesRef = Database.database().reference().child(Constants.RootFolders.likes)
+    let targetsRef = Database.database().reference().child(Constants.RootFolders.Targets)
     
-    func saveLikeToDatabase(targetId: String, userId: String, onSuccess: @escaping () -> Void, onError: @escaping (String) -> Void) {
-        
+    func updateUsersLikesFor(targetId: String, userId: String, onSuccess: @escaping () -> Void, onError: @escaping (String) -> Void) {
+        let timestamp = Int(Date().timeIntervalSince1970)
+        likesRef.child(targetId).child(userId).observeSingleEvent(of: .value) { (snapshot) in
+            if let dict = snapshot.value as? [String: Any] {
+                
+            }
+        } withCancel: { (error) in
+            onError(error.localizedDescription)
+        }
+
+    }
+    
+    func incrementLikes(targetId: String, todaysLikeExcists: Bool, onSuccess: @escaping (TargetModel) -> Void, onError: @escaping (String) -> Void) {
+        targetsRef.child(targetId).runTransactionBlock({ (currentData: MutableData) -> TransactionResult in
+            if var target = currentData.value as? [String : AnyObject], let uid = Api.user.currentUser?.uid {
+                var likeCount = target["likeCount"] as? Int ?? 0
+                if todaysLikeExcists {
+                    // Unstar the post and remove self from stars
+                    likeCount -= 1
+                } else {
+                    // Star the post and add self to stars
+                    likeCount += 1
+                }
+                target[Constants.Target.likesCount] = likeCount as AnyObject?
+                
+                // Set value and report transaction success
+                currentData.value = target
+                
+                return TransactionResult.success(withValue: currentData)
+            }
+            return TransactionResult.success(withValue: currentData)
+        }) { (error, committed, snapshot) in
+            if let error = error {
+                onError(error.localizedDescription)
+            }
+            if let dict = snapshot?.value as? [String: Any] {
+                let target = TargetModel.transformDataToTarget(dict: dict, id: targetId)
+                onSuccess(target)
+            }
+        }
     }
 }
