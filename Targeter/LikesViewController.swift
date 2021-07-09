9be3952c877ade2ea0d6ba8e49d12c23eddca4b4
Alexander Kolbin <kolbin_aa@yahoo.com>
2021-07-09 17:45:23 -0400

    feat: Create LikesViewController storyboard and class

---
+++ Targeter/LikesViewController.swift
@@ -0,0 +1,13 @@
+//
+//  LikesViewController.swift
+//  Targeter
+//
+//  Created by Alexander Kolbin on 7/9/21.
+//  Copyright © 2021 Alder. All rights reserved.
+//
+
+import Foundation
+
+class LikesViewController: UIViewController  {
+    
+}
