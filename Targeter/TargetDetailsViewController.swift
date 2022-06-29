9db6b40aa696de74ffb2cd3c0fd73d44078d96bd
Alexander Kolbin <kolbin_aa@yahoo.com>
2022-06-29 04:07:48 -0400

    feat: Create TargetDetailsViewController file and class

---
+++ Targeter/TargetDetailsViewController.swift
@@ -0,0 +1,13 @@
+//
+//  TargetDetailsViewController.swift
+//  Targeter
+//
+//  Created by Alexander Kolbin on 6/29/22.
+//  Copyright © 2022 Alder. All rights reserved.
+//
+
+import Foundation
+
+class TargetDetailsViewController: UIViewController {
+    
+}
